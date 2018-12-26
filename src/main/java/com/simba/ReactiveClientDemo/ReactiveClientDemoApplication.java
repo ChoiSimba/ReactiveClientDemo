bccf5b9325db3b3a4f90f26ea67eefa88a3ea9a7
최순진 <simba@wemakeprice.com>
2018-12-26 17:07:50 +0900

    Init

---
+++ src/main/java/com/simba/ReactiveClientDemo/ReactiveClientDemoApplication.java
@@ -0,0 +1,14 @@
+package com.simba.ReactiveClientDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveClientDemoApplication.class, args);
+	}
+
+}
+
